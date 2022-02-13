64d0f53c0941999e1e293dca8aac8c627e0d86cd
array-in-a-matrix <nova.neutrino@protonmail.com>
2022-02-13 17:58:13 -0500

    randomly msg  one of the most recent chats

--- index.py
+++ index.py
@@ -1,10 +1,11 @@
 import sys
+import json
 from markov import markov
 from time import sleep
+from numpy.random import randint
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-import json
 
 message, cmd_length = None, None
 try:
@@ -17,55 +18,41 @@ try:
 except IndexError:
     print("No length given.")
 
-"""
-cmd_length = None
-try:
-    cmd_length = sys.argv[1]
-
-    if cmd_length.isdigit():
-        print(markov(int(cmd_length) - 1))
-    else:
-        print(markov())
-except IndexError:
-    print(markov())
-"""
 
 with open('login.json', 'r') as file:
     json_object = json.load(file)
 
 username = json_object['username']
 password = json_object['password']
+recent = json_object['recent']
 
 browser = webdriver.Firefox()
 browser.implicitly_wait(5)
 browser.get('https://www.instagram.com/')
-sleep(3)
+sleep(5)
 
-#!! is deprecated
-#!! browser.find_element_by_css_selector("input[name='username']").send_keys(username)
 browser.find_element(By.CSS_SELECTOR, "[name='username']").send_keys(username)
-
-#!! browser.find_element_by_css_selector("input[name='password']").send_keys(password)
 browser.find_element(By.CSS_SELECTOR, "[name='password']").send_keys(password)
-
-#!! browser.find_element_by_xpath("//button[@type='submit']").click()
 browser.find_element(By.XPATH, "//button[@type='submit']").click()
-
-sleep(3)
+sleep(5)
 
 browser.get('https://www.instagram.com/direct/inbox/')
-sleep(3)
+sleep(5)
 
 browser.find_element(By.CSS_SELECTOR, "button.aOOlW:nth-child(2)").click()
+sleep(5)
 
-# browser.find_element(By.XPATH, "/html/body/div[1]/section/div/div[2]/div/div/div[1]/div[2]/div/div/div/div/div[1]/a").click()
-sleep(3)
+# browser.find_element(
+#     By.XPATH, "/html/body/div[1]/section/div/div[2]/div/div/div[1]/div[2]/div/div/div/div/div[1]/a").click()
+# selects most recent chat                                                                      ^ chat order
 
-browser.find_element(By.XPATH, "/html/body/div[1]/section/div/div[2]/div/div/div[1]/div[2]/div/div/div/div/div[5]/a").click()
+while True:
+    chat = 1 + randint(recent)
+    browser.find_element(
+        By.XPATH, f"/html/body/div[1]/section/div/div[2]/div/div/div[1]/div[2]/div/div/div/div/div[{chat}]").click()
 
-sleep(3)
+    sleep(5)
 
-while true:
     try:
         cmd_length = sys.argv[1]
         if cmd_length.isdigit():
@@ -75,10 +62,11 @@ while true:
     except IndexError:
         message = markov()
 
+    sleep(5)
 
     browser.find_element(By.CSS_SELECTOR, "[placeholder='Message...']").send_keys(
         message + Keys.ENTER)
 
-    sleep(10)
+    sleep(20)
 
 browser.close()
